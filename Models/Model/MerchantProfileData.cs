19f76e6019423145170a5d4f9abcca4ddd077ef1
Amiin Majiid Nugroho <amiinmajiidnugroho@gmail.com>
2022-05-23 17:29:29 +0700

    update2 - user costumer and merchant profile

--- Models/Model/MerchantProfileData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Model.Model
-{
-    public class MerchantProfileData
-    {
-        public int Id { get; set; }
-        public int UserId { get; set; }
-        public string CompanyName { get; set; } = null!;
-    }
-}
